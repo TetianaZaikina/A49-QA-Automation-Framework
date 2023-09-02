aef8da3b4bba9dcf8db4df917afe1206b8ba902c
TetianaZaikina <pchelka2403@yahoo.com>
2023-09-02 11:33:07 -0400

    Homework18
    -corrected the mistakes and applied changes
    - tried to stick to Natalia's file on GitHub
    -the code is not working

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest{
-    @Test()
+    @Test
     public void PlaySong () throws InterruptedException {
         navigateToPage();
         provideEmail("pchelka240386@gmail.com");
@@ -15,28 +15,30 @@ public class Homework18 extends BaseTest{
         checkSongIsPlaying();
     }
 
-    private void clickSubmit() {
+    public void navigateToPage() {
     }
 
-    private void providePassword(String te$t$tudent) {
+    private void provideEmail(String email) {
+        WebElement emailField=driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys(email);
     }
-
-    private void provideEmail(String "pchelka240386@gmail.com"String mail) {
+    private void providePassword(String password) {
+        WebElement passwordField=driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys(password);
     }
-
-    private void navigateToPage() {
+    public void clickSubmit() {
     }
-
-    private void checkSongIsPlaying() {
-        WebElement soundBar= driver.findElement(By.cssSelector("[data-testid='sound-bar-play']"));
-        Assert.assertTrue(soundBar.isDisplayed());
-    }
-
-    private void clickPlayNextBtn() throws InterruptedException {
+        private void clickPlayNextBtn() throws InterruptedException {
         WebElement playNextBtn= driver.findElement(By.cssSelector("[data-testid='play-next-btn']"));
         playNextBtn.click();
         WebElement playBtn= driver.findElement(By.cssSelector("[data-testid='play-btn']"));
         playBtn.click();
         Thread.sleep(5000);
+    }
+    public void checkSongIsPlaying() {
+        WebElement soundBar= driver.findElement(By.cssSelector("[data-testid='sound-bar-play']"));
+        Assert.assertTrue(soundBar.isDisplayed());
     }
 }
